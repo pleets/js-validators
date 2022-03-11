052af52ec255b96f72be19afce6bdeeaefde9a85
Darío Rivera <fermius.us@gmail.com>
2022-03-11 17:44:00 -0500

    style: eslint fixes

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-export default class Calculator
-{
-    sum($a, $b)
-    {
-        return $a + $b;
-    }
+export default class Calculator {
+  sum($a, $b) {
+    return $a + $b;
+  }
 }
---
+++ src/Digits.js
@@ -0,0 +1,11 @@
+class Digits {
+  constructor(value) {
+    this.value = value;
+  }
+
+  isValid(value) {
+    return true;
+  }
+}
+
+export default Digits;
